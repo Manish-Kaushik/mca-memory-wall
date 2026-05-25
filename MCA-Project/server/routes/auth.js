ae55a3e83b05510addc03c1a058d1beaa4d4072e
Manish Kaushik <107453887+Manish-Kaushik@users.noreply.github.com>
2026-05-25 14:40:35 +0530

    Update auth.js

--- MCA-Project/server/routes/auth.js
+++ MCA-Project/server/routes/auth.js
@@ -115,56 +115,6 @@ router.post('/login', async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-// ================= UPDATE PROFILE =================
-router.put('/profile', protect, async (req, res) => {
-  try {
-    const { name, phone, linkedin, instagram, github } = req.body;
-
-    const user = await User.findById(req.user._id);
-
-    if (!user) {
-      return res.status(404).json({
-        message: 'User not found'
-      });
-    }
-
-    user.name = name || user.name;
-    user.phone = phone || user.phone;
-    user.linkedin = linkedin || user.linkedin;
-    user.instagram = instagram || user.instagram;
-    user.github = github || user.github;
-
-    const updatedUser = await user.save();
-
-    const token = jwt.sign(
-      { id: updatedUser._id },
-      process.env.JWT_SECRET,
-      { expiresIn: '30d' }
-    );
-
-    res.json({
-      token,
-      user: {
-        _id: updatedUser._id,
-        name: updatedUser.name,
-        email: updatedUser.email,
-        role: updatedUser.role,
-        avatar: updatedUser.profileImage,
-        phone: updatedUser.phone,
-        linkedin: updatedUser.linkedin,
-        instagram: updatedUser.instagram,
-        github: updatedUser.github
-      }
-    });
-
-  } catch (error) {
-    console.error(error);
-
-    res.status(500).json({
-      message: error.message
-    });
-  }
-});
 
 // ================= UPLOAD PROFILE PIC =================
 router.put('/profile/pic', protect, upload.single('image'), async (req, res) => {
