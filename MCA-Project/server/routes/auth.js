c8490f8798436bf44a114c477890131e3878f66d
Manish Kaushik <107453887+Manish-Kaushik@users.noreply.github.com>
2026-05-25 14:25:58 +0530

    Enhance login route with request body logging
    Added logging for request body during login process and removed unnecessary console logs.

--- MCA-Project/server/routes/auth.js
+++ MCA-Project/server/routes/auth.js
@@ -72,6 +72,8 @@ router.post('/register', async (req, res) => {
 // ================= LOGIN =================
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
+  console.log(req.body);
+
 
   try {
     const user = await User.findOne({ email });
@@ -108,12 +110,7 @@ router.post('/login', async (req, res) => {
   } catch (error) {
     console.error(error);
 
-    console.log(req.body);
-console.log("Entered:", password);
-console.log("DB hash:", user.password);
-
-const match = await bcrypt.compare(password, user.password);
-console.log("Match:", match);
+    
     
     res.status(500).json({
       message: error.message
