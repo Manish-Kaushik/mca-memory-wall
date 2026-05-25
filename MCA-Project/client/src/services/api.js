720260ed6e58c67dbc65b5d28d2f10b1322d47eb
Manish Kaushik <107453887+Manish-Kaushik@users.noreply.github.com>
2026-05-25 12:30:28 +0530

    Update api.js

--- MCA-Project/client/src/services/api.js
+++ MCA-Project/client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
+const api = axios.create({ baseURL: 'https://mca-memory-wall-mxkv.onrender.com/api' });
 api.interceptors.request.use((config) => {
   const userInfo = JSON.parse(localStorage.getItem('userInfo'));
   if (userInfo?.token) config.headers.Authorization = `Bearer ${userInfo.token}`;
